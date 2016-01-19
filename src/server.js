32559ed4d7283f05063c39278441870e77a21a61
Max Polun <maxpolun@gmail.com>
2016-01-18 22:01:54 -0500

    add db middleware

--- src/server.js
+++ src/server.js
@@ -4,13 +4,16 @@ let koaLogger = require('koa-logger')
 let router = require('koa-router')()
 let koaBody = require('koa-body')()
 
+let dbMiddleware = require('./middlewares/db')
+
 router.get('/', koaBody, function * () {
-  this.body = {hello: 'world'}
+  this.body = (yield this.db.query('SELECT * FROM users')).rows
 })
 
 let app = koa()
 
 app.use(koaLogger())
+    .use(dbMiddleware(process.env.DATABASE_URL || 'postgres://podqueue@localhost/podqueue-dev'))
     .use(router.routes())
     .use(router.allowedMethods())
 
